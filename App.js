83ef6286f797123c185da32885c1e732b8593cec
xahid71 <jahid.info97@gmail.com>
2024-01-16 16:04:56 +0600

    Add today's task section to App.js

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Task  from './components/Tasks';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* Today's Task */}
+     <View style={styles.taskWrapper}>
+      <Text style={styles.sectionTitle}>Today's Task</Text>
+      <View style={styles.items}>
+        <Task text ={"Task 1"}/>
+        <Task text ={"Task 2"}/>
+        <Task text ={"Task 3"}/>
+        <Task text ={"Task 4"}/>
+
+      </View>
+     </View>
     </View>
   );
 }
@@ -13,8 +23,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // backgroundColor: '#fff',
+    backgroundColor: '#E8EAED',
   },
+  taskWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold', 
+  },
+  items: {
+    marginTop: 30,
+  }
+
+    
 });
